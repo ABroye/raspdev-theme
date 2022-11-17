ca52308300a03f3ef2b3a6e3f74f318ee4f06b3e
Alain BROYE <97102638+ABroye@users.noreply.github.com>
2022-11-17 19:38:11 +0100

    Update front-page.php
    Add a margin-top

--- raspdev/front-page.php
+++ raspdev/front-page.php
@@ -7,7 +7,7 @@
 
         <?php while (have_posts()) : the_post() ?>
 
-            <div class="col-lg-3 col-md-6 col-sm-6 mb-4 mb-md-4">
+            <div class="col-lg-3 col-md-6 col-sm-6 my-4 my-md-4">
                 <div class="card card01 shadow-lg border-0 h-100">
                     <?php the_post_thumbnail('medium', ['class' => 'img-fluid w-100', 'alt' => '']) ?>
                     <div class="card-body bg-dark text-white d-flex flex-column border-top color-card" style="background-color: <?= get_theme_mod('card_background'); ?>!important">
